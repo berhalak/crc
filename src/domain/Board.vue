f28b1bf0ce59e7993a492f566431e2f39dabc48d
Jarosław Sadziński <jaroslaw.sadzinski@gmail.com>
2024-04-29 09:32:04 +0200

    Add version number to Board.vue and Position.vue

--- src/domain/Board.vue
+++ src/domain/Board.vue
@@ -7,5 +7,7 @@
 			@click="save()"
 			class="absolute text-green-900 bg-green-400 active:bg-red-500"
 		>Save</Button>
+
+    <div class="absolute bottom-0 right-0 text-xs">ver1</div>
 	</div>
 </template>
